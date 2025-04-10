08e6405074b94c1208759a2df88eb1de37d001fc
hniii98 <752708928@qq.com>
2025-04-10 15:31:37 +0900

    [add] polish Decode lab by setting the rest segs light off

--- Decode/vsrc/top.v
+++ Decode/vsrc/top.v
@@ -1,13 +1,23 @@
 module top(
     input [7:0] switches,
     input en,
-    output [2:0] decode83_out,
-    output [6:0] seg
+    output [2:0] decode83_out, 
+    output [6:0] seg0,
+    output [7:0] seg1, seg2, seg3, seg4, seg5, seg6, seg7
 
 );
 
     wire decoder_en;
     
+    // off all rest seg
+    assign seg1 = 8'b11111111;
+    assign seg2 = 8'b11111111;
+    assign seg3 = 8'b11111111;
+    assign seg4 = 8'b11111111;
+    assign seg5 = 8'b11111111;
+    assign seg6 = 8'b11111111;
+    assign seg7 = 8'b11111111;
+
     decode83 u_decoder (
         .in(switches),
         .out(decode83_out),
@@ -19,7 +29,7 @@ module top(
     bcd7seg u_seg7(
         .bin_in(decode83_out),
         .en(final_en),
-        .seg_out(seg)
+        .seg_out(seg0)
 
     );
 
